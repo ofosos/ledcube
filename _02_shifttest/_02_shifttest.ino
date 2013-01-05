88fa92dee45de5c8260928105bf815e4a6d38398
Mark Meyer <ofosos@gmail.com>
2013-01-05 16:40:18 +0100

    fix sketch comment

--- _02_shifttest/_02_shifttest.ino
+++ _02_shifttest/_02_shifttest.ino
@@ -1,6 +1,6 @@
 /*
   shift test
-  Test layer control on my 4x4x4 led cube. Layer transistors are directly connect to pin 0-3
+  Test shift register. Connected 4 blue leds to the outputs of an 74hc595.
  
   pin 0 - output enable
   pin 1 - clock
@@ -13,20 +13,20 @@ int cnt = 0;
 
 void setup() {
   DDRD = 0x7;
-  PORTD = 0x0;
+  PORTD = 0x1;
 }
 
 void loop() {
   int x = cnt;
   
-  digitalWrite(0, 0); // disable output
+  digitalWrite(0, 1); // disable output
   
   digitalWrite(2,x); // write 1-bit data
   
   digitalWrite(1, 1); // clock pulse
   digitalWrite(1, 0);
   
-  digitalWrite(0, 1); // enable output
+  digitalWrite(0, 0); // enable output
   
   cnt = ~ cnt;
   
